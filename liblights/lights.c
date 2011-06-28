a2d8e2d26ec63b5767b1110ebd6e647e90e341ef
Tomasz Figa <tomasz.figa@gmail.com>
2011-06-29 01:02:09 +0200

    Update liblights for 2.6.38 kernel.
    Change-Id: I46cc2f1eb5687c33a7da6130e75c6574271dff83

--- liblights/lights.c
+++ liblights/lights.c
@@ -14,206 +14,116 @@
  * limitations under the License.
  */
 
-
-// #define LOG_NDEBUG 0
 #define LOG_TAG "lights"
-
 #include <cutils/log.h>
-
 #include <stdint.h>
 #include <string.h>
-#include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
-
 #include <sys/ioctl.h>
 #include <sys/types.h>
-
 #include <hardware/lights.h>
-
-/******************************************************************************/
 
 static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
-enum lights {
-    null
-};
+char const *const LCD_FILE = "/sys/class/backlight/s6d05a-lcd/brightness";
 
-char const*const LCD_FILE
-        = "/sys/class/leds/lcd-backlight/brightness";
-
-/**
- * device methods
- */
-
-void init_globals(void)
+static int write_int(char const *path, int value)
 {
-    // init the mutex
-    pthread_mutex_init(&g_lock, NULL);
+	int fd;
+	static int already_warned;
+
+	already_warned = 0;
+
+	LOGV("write_int: path %s, value %d", path, value);
+	fd = open(path, O_RDWR);
+
+	if (fd >= 0) {
+		char buffer[20];
+		int bytes = sprintf(buffer, "%d\n", value);
+		int amt = write(fd, buffer, bytes);
+		close(fd);
+		return amt == -1 ? -errno : 0;
+	} else {
+		if (already_warned == 0) {
+			LOGE("write_int failed to open %s\n", path);
+			already_warned = 1;
+		}
+		return -errno;
+	}
 }
 
-static int
-write_int(char const* path, int value)
+static int rgb_to_brightness(struct light_state_t const *state)
 {
-    int fd;
-    static int already_warned = 0;
+	int color = state->color & 0x00ffffff;
 
-    fd = open(path, O_RDWR);
-    if (fd >= 0) {
-        char buffer[20];
-        int bytes = sprintf(buffer, "%d\n", value);
-        int amt = write(fd, buffer, bytes);
-        close(fd);
-        return amt == -1 ? -errno : 0;
-    } else {
-        if (already_warned == 0) {
-            LOGE("write_int failed to open %s\n", path);
-            already_warned = 1;
-        }
-        return -errno;
-    }
+	return ((77*((color>>16) & 0x00ff))
+		+ (150*((color>>8) & 0x00ff)) + (29*(color & 0x00ff))) >> 8;
 }
 
-static int
-is_lit(struct light_state_t const* state)
+static int set_light_backlight(struct light_device_t *dev,
+			struct light_state_t const *state)
 {
-    return state->color & 0x00ffffff;
+	int err = 0;
+	int brightness = rgb_to_brightness(state);
+
+	pthread_mutex_lock(&g_lock);
+	err = write_int(LCD_FILE, brightness);
+
+	pthread_mutex_unlock(&g_lock);
+	return err;
 }
 
-static int
-rgb_to_brightness(struct light_state_t const* state)
+static int close_lights(struct light_device_t *dev)
 {
-    int color = state->color & 0x00ffffff;
-    return ((77*((color>>16)&0x00ff))
-            + (150*((color>>8)&0x00ff)) + (29*(color&0x00ff))) >> 8;
+	LOGV("close_light is called");
+	if (dev)
+		free(dev);
+
+	return 0;
 }
 
-static int
-set_light_backlight(struct light_device_t* dev,
-        struct light_state_t const* state)
+static int open_lights(const struct hw_module_t *module, char const *name,
+						struct hw_device_t **device)
 {
-    int err = 0;
-    int brightness = rgb_to_brightness(state);
-    pthread_mutex_lock(&g_lock);
-    err = write_int(LCD_FILE, brightness);
-    pthread_mutex_unlock(&g_lock);
-    return err;
+	int (*set_light)(struct light_device_t *dev,
+		struct light_state_t const *state);
+
+	LOGV("open_lights: open with %s", name);
+
+	if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
+		set_light = set_light_backlight;
+	else
+		return -EINVAL;
+
+	pthread_mutex_init(&g_lock, NULL);
+
+	struct light_device_t *dev = malloc(sizeof(struct light_device_t));
+	memset(dev, 0, sizeof(*dev));
+
+	dev->common.tag = HARDWARE_DEVICE_TAG;
+	dev->common.version = 0;
+	dev->common.module = (struct hw_module_t *)module;
+	dev->common.close = (int (*)(struct hw_device_t *))close_lights;
+	dev->set_light = set_light;
+
+	*device = (struct hw_device_t *)dev;
+
+	return 0;
 }
-
-
-static int
-set_light_buttons(struct light_device_t* dev,
-        struct light_state_t const* state)
-{
-    // no button lights
-    return 0;
-}
-
-static int
-set_light_locked(enum lights light, unsigned int status, unsigned int blink)
-{
-    // no LEDs
-    return 0;
-}
-
-static int
-set_light_battery(struct light_device_t* dev,
-        struct light_state_t const* state)
-{
-    // no LEDs
-    return 0;
-}
-
-static int
-set_light_notification(struct light_device_t* dev,
-        struct light_state_t const* state)
-{
-    // no LEDs
-    return 0;
-}
-
-static int
-set_light_attention(struct light_device_t* dev,
-        struct light_state_t const* state)
-{
-    // no LEDs
-    return 0;
-}
-
-/** Close the lights device */
-static int
-close_lights(struct light_device_t *dev)
-{
-    if (dev) {
-        free(dev);
-    }
-    return 0;
-}
-
-
-/******************************************************************************/
-
-/**
- * module methods
- */
-
-/** Open a new instance of a lights device using name */
-static int open_lights(const struct hw_module_t* module, char const* name,
-        struct hw_device_t** device)
-{
-    int (*set_light)(struct light_device_t* dev,
-            struct light_state_t const* state);
-
-    if (0 == strcmp(LIGHT_ID_BACKLIGHT, name)) {
-        set_light = set_light_backlight;
-    }
-    else if (0 == strcmp(LIGHT_ID_BUTTONS, name)) {
-        set_light = set_light_buttons;
-    }
-    else if (0 == strcmp(LIGHT_ID_BATTERY, name)) {
-        set_light = set_light_battery;
-    }
-    else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name)) {
-        set_light = set_light_notification;
-    }
-    else if (0 == strcmp(LIGHT_ID_ATTENTION, name)) {
-        set_light = set_light_attention;
-    }
-    else {
-        return -EINVAL;
-    }
-
-    pthread_once(&g_init, init_globals);
-
-    struct light_device_t *dev = malloc(sizeof(struct light_device_t));
-    memset(dev, 0, sizeof(*dev));
-
-    dev->common.tag = HARDWARE_DEVICE_TAG;
-    dev->common.version = 0;
-    dev->common.module = (struct hw_module_t*)module;
-    dev->common.close = (int (*)(struct hw_device_t*))close_lights;
-    dev->set_light = set_light;
-
-    *device = (struct hw_device_t*)dev;
-    return 0;
-}
-
 
 static struct hw_module_methods_t lights_module_methods = {
-    .open =  open_lights,
+	.open =  open_lights,
 };
 
-/*
- * The lights Module
- */
 const struct hw_module_t HAL_MODULE_INFO_SYM = {
-    .tag = HARDWARE_MODULE_TAG,
-    .version_major = 1,
-    .version_minor = 0,
-    .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "i5700 Spica lights Module",
-    .author = "Google, Inc.",
-    .methods = &lights_module_methods,
+	.tag = HARDWARE_MODULE_TAG,
+	.version_major = 1,
+	.version_minor = 0,
+	.id = LIGHTS_HARDWARE_MODULE_ID,
+	.name = "lights Module",
+	.author = "Google, Inc.",
+	.methods = &lights_module_methods,
 };
