6e2718efbe8d2cfc32ed694c45a14ad7d0563816
Tomasz Figa <tomasz.figa@gmail.com>
2011-06-29 15:09:03 +0200

    Makefiles fixes for liblights.
    Change-Id: I767a6b04804c9300e28714b77938e5b09dd3dbfa

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(filter crespo crespo4g,$(TARGET_DEVICE)),)
-
 LOCAL_PATH:= $(call my-dir)
 # HAL module implemenation, not prelinked and stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
@@ -26,10 +24,9 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
 LOCAL_SHARED_LIBRARIES := liblog
 
-LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := lights.GT-I5700
 
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
-endif
