549133cbbc853e807088bf825f7607ad9089bf4b
Tomasz Figa <tomasz.figa@gmail.com>
2011-11-19 13:09:10 +0100

    Allow bcmdhd driver to poll the WLAN chip in case of missed interrupts.
    Change-Id: I9897b81d34a5af2c9c9e7a0e8b9c7f52cbb4a55d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,10 +89,10 @@ TARGET_PROVIDES_LIBRIL := true
 WPA_SUPPLICANT_VERSION := VER_0_6_X
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
 BOARD_WLAN_DEVICE := bcm4329
-WIFI_DRIVER_MODULE_PATH     := "/lib/modules/bcmdhd.ko"
-WIFI_DRIVER_FW_STA_PATH     := "/system/etc/fw_bcm4325.bin"
-WIFI_DRIVER_MODULE_NAME     :=  "bcmdhd"
-WIFI_DRIVER_MODULE_ARG      :=  "firmware_path=/system/etc/fw_bcm4325.bin nvram_path=/system/etc/nvram.txt"
+WIFI_DRIVER_MODULE_PATH := "/lib/modules/bcmdhd.ko"
+WIFI_DRIVER_FW_STA_PATH := "/system/etc/fw_bcm4325.bin"
+WIFI_DRIVER_MODULE_NAME := "bcmdhd"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/fw_bcm4325.bin nvram_path=/system/etc/nvram.txt dhd_watchdog_ms=10 dhd_poll=1"
 
 # GPS
 BOARD_GPS_LIBRARIES := libsecgps libsecril-client
